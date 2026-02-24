61780249ddbbc6a37116f7c10e014d28916431e2
urever <83172845+urever@users.noreply.github.com>
2026-02-24 13:21:35 +0800

    Update op2.sh

--- diy/mt798x/op2.sh
+++ diy/mt798x/op2.sh
@@ -54,8 +54,6 @@ sed -i "3iuci commit istore" package/emortal/default-settings/files/99-default-s
 sed -i "s/MT7986_AX6000_2.4G/EasyWrt-2.4G/g" package/mtk/drivers/wifi-profile/files/mt7986/mt7986-ax6000.dbdc.b0.dat
 sed -i "s/MT7986_AX6000_5G/EasyWrt-5G/g" package/mtk/drivers/wifi-profile/files/mt7986/mt7986-ax6000.dbdc.b1.dat
 
-sed -i "s/MT7981_AX3000_2.4G/EasyWrt-2.4G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
-sed -i "s/MT7981_AX3000_5G/EasyWrt-5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
 
 ##New WiFi
 sed -i "s/ImmortalWrt-2.4G/EasyWrt-2.4G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
